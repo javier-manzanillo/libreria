a8fe44b64ddd50b27f1ad6000f715bad73ba601d
javier <javiermanzanillo29@gmail.com>
2020-11-19 19:53:35 -0500

    ajajja
    ajjjaja

---
+++ libreriaa/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBi1jo5RXUMRdK9t0Vlpd5aXQB2_6uhzWc",
+    authDomain: "libreria-ng.firebaseapp.com",
+    databaseURL: "https://libreria-ng.firebaseio.com",
+    projectId: "libreria-ng",
+    storageBucket: "libreria-ng.appspot.com",
+    messagingSenderId: "567092653273",
+    appId: "1:567092653273:web:8a185bd0b354d0007ea2c8"
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
